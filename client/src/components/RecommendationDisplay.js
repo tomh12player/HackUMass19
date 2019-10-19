475d939cdbaf5ff5dd1cff2a6bb9c2313c37ba61
tomh12player <thomascheng@umass.edu>
2019-10-19 19:08:16 -0400

    autoplay song if it has preview

--- client/src/components/RecommendationDisplay.js
+++ client/src/components/RecommendationDisplay.js
@@ -8,6 +8,11 @@ export default class RecommendationDisplay extends React.Component {
                 <h1>{this.props.track.name}</h1>
                 <h2>{this.props.track.artists[0].name}</h2>
                 <h3>{this.props.track.album.name}</h3>
+                <div>
+                <React.Fragment key={"https://api.spotify.com/v1/tracks/1TKYPzH66GwsqyJFKFkBHQ"}>
+                    <video src={"https://api.spotify.com/v1/tracks/1TKYPzH66GwsqyJFKFkBHQ"} autoPlay />
+                </React.Fragment>
+                </div>
             </div>
         )
     }
